8470f3b7ac5fbe12b69738cb9d732a1a3ff117c9
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-09-15 01:26:07 +0900

    HUD Scene の追加

---
+++ src/hud.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+# Notifies `Main` node that the button has been pressed
+signal start_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+
+	# 一時的なタイマーを開始して終了まで待つ
+	await get_tree().create_timer(1.0).timeout
+
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout() -> void:
+	$MessageLabel.hide()
